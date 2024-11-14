d9cb66708988e918077b470910689d4d49f1b4c7
Christopher Young <christopheryoung@MacBook123.local>
2024-11-13 18:33:43 -0800

    scroll-to-top on route change

---
+++ src/components/utils/ScrollToTop.tsx
@@ -0,0 +1,20 @@
+// React
+import { useEffect } from 'react';
+// Routing
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, [location]);
+
+  return null;
+}
+
+export default ScrollToTop;
--- src/components/wrappers/Layout.tsx
+++ src/components/wrappers/Layout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 // Components
 import Navigationbar from "../navigation/NavigationBar";
 import Footer from "../navigation/Footer";
+import ScrollToTop from "../utils/ScrollToTop";
 // Context
 import AuthProvider from "../../hooks/AuthProvider";
 // Animation
@@ -16,6 +17,7 @@ const Layout = () => {
         <div id="layout">
           <Navigationbar/>
           <div id="layout-content">
+            <ScrollToTop/>
             <Outlet/>
           </div>
           <Footer/>
