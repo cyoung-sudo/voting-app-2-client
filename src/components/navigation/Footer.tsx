49f5acbbebf93e386d22db54319ae7fe908588ab
Christopher Young <christopheryoung@MacBook123.local>
2024-11-16 19:05:23 -0800

    updated icon & metadata

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 const Footer = () => {
   return(
     <footer id="footer">
-      Footer
+      <a href="https://www.flaticon.com/free-icons/voting" title="voting icons">Voting icons created by IconBaandar - Flaticon</a>
     </footer>
   )
 };
